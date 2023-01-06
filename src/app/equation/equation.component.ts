c18f9a90e883bc42f6a42a9520c2d2a8c84f65be
snehal patel <snpatel.dev@gmail.com>
2023-01-06 16:02:40 -0500

    #3 using RxJs

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { delay, filter, scan } from 'rxjs/operators';
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -8,6 +9,7 @@ import { MathValidators } from '../math-validators';
   styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent implements OnInit {
+  secondsperSolution = 0;
   mathForm = new FormGroup(
     {
       firstValue: new FormControl(this.randomNumber()),
@@ -26,7 +28,29 @@ export class EquationComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.mathForm.statusChanges.subscribe((value) => console.log(value));
+    this.mathForm.statusChanges
+      .pipe(
+        filter((value) => value === 'VALID'),
+        delay(200),
+        scan(
+          (acc) => {
+            return {
+              numberSolved: acc.numberSolved + 1,
+              startTime: acc.startTime,
+            };
+          },
+          { numberSolved: 0, startTime: new Date() }
+        )
+      )
+      .subscribe(({ numberSolved, startTime }) => {
+        this.secondsperSolution =
+          (new Date().getTime() - startTime.getTime()) / numberSolved / 1000;
+        this.mathForm.setValue({
+          firstValue: this.randomNumber(),
+          secondValue: this.randomNumber(),
+          answer: '',
+        });
+      });
   }
 
   randomNumber() {
