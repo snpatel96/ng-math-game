2b1dbec9866f7a4ec6bb91b9c8e305e952ebe55d
snehal patel <snpatel.dev@gmail.com>
2023-01-06 15:20:44 -0500

    #1 custom validators

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-equation',
   templateUrl: './equation.component.html',
-  styleUrls: ['./equation.component.css']
+  styleUrls: ['./equation.component.css'],
 })
 export class EquationComponent {
+  mathForm = new FormGroup(
+    {
+      firstValue: new FormControl(this.randomNumber()),
+      secondValue: new FormControl(this.randomNumber()),
+      answer: new FormControl(''),
+    },
+    [
+      (form: AbstractControl) => {
+        const { firstValue, secondValue, answer } = form.value;
+        if (firstValue + secondValue === parseInt(answer)) {
+          return null;
+        }
+        return { addition: true };
+      },
+    ]
+  );
 
+  get firstValue() {
+    return this.mathForm.value.firstValue;
+  }
+
+  get secondValue() {
+    return this.mathForm.value.secondValue;
+  }
+
+  randomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
 }
